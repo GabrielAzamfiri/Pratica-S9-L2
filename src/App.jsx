414c422dabfe4ae8abf9e1b583eac2d29625bc43
GabrielAzamfiri <167236687+GabrielAzamfiri@users.noreply.github.com>
2024-06-25 14:22:24 +0200

    add bootstrap e react bootstrap
    prova add navbar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./App.css";
+import TopBar from "./components/TopBar";
+
+
+function App() {
+  return <div className="App">
+    <TopBar/>
+  </div>;
+}
+
+export default App;
